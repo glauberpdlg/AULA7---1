eda0ff4851568012dac593bb4c7eab5e393305a7
Glauber Mathias <glauber.pdlg@gmail.com>
2025-05-16 20:33:16 -0300

    Update desafio2.py

--- desafio2.py
+++ desafio2.py
@@ -1,9 +1,11 @@
 
 def mostrar_tabuada(numero):
-    for i in range(1, 11):
+    for i in range(0, 11):
         resultado = numero * i
         print(f"{numero} x {i} = {resultado}")
 
-mostrar_tabuada(2)
+
+operacao = int(input("Digite o número para saber a multiplicação: "))
+mostrar_tabuada(operacao)
 
     
